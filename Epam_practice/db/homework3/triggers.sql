5ed7f30660a82f1f7b565acebe65feccfb654afa
Dana <pavlenko-dana18@ukr.net>
2018-02-05 23:59:58 +0200

    added more triggers

--- Epam_practice/db/homework3/triggers.sql
+++ Epam_practice/db/homework3/triggers.sql
@@ -2,7 +2,7 @@ SET SQL_SAFE_UPDATES = 0;
 
 USE StoredPr_DB; 
 
--- insert with foreign keys
+-- insert 
 
 DELIMITER //
 CREATE TRIGGER before_insert_employee
@@ -14,7 +14,7 @@ BEGIN
 		SET MESSAGE_TEXT = "There is no such post!";
 	END IF;
     
-    IF (new.pharmacy_id NOT IN (SELECT pharmacy_id FROM pharmacy))
+    IF (new.pharmacy_id NOT IN (SELECT id FROM pharmacy))
     THEN SET new.pharmacy_id = NULL;
     END IF;
 END //
@@ -70,7 +70,7 @@ END //
 
 DELIMITER ;
 
--- update post
+-- update 
 
 DELIMITER //
 CREATE TRIGGER no_post_update
@@ -79,6 +79,72 @@ ON post FOR EACH ROW
 BEGIN
         SIGNAL SQLSTATE '45000'
 		SET MESSAGE_TEXT = "You cannot update any post!";
+END //
+
+DELIMITER ;
+
+DELIMITER //
+CREATE TRIGGER before_update_employe
+BEFORE UPDATE
+ON employee FOR EACH ROW
+BEGIN
+	IF (new.post NOT IN (SELECT post FROM post))
+    THEN SIGNAL SQLSTATE '45000'
+		SET MESSAGE_TEXT = "There is no such post!";
+	END IF;
+    
+    IF (new.pharmacy_id NOT IN (SELECT id FROM pharmacy))
+    THEN SET new.pharmacy_id =  NULL;
+    END IF;
+    
+END //
+
+DELIMITER ;
+
+DELIMITER //
+CREATE TRIGGER before_update_medicine_zone
+BEFORE UPDATE
+ON medicine_zone FOR EACH ROW
+BEGIN
+	IF (new.medicine_id NOT IN (SELECT id FROM medicine)) 
+    THEN SIGNAL SQLSTATE '45000'
+		SET MESSAGE_TEXT = "There is no such medicine!";
+	END IF;
+    
+    IF (new.zone_id NOT IN (SELECT id FROM zone)) 
+    THEN SIGNAL SQLSTATE '45000'
+		SET MESSAGE_TEXT = "There is no such zone!";
+	END IF;
+END //
+
+DELIMITER ;
+
+DELIMITER //
+CREATE TRIGGER before_update_pharmacy
+BEFORE UPDATE
+ON pharmacy FOR EACH ROW
+BEGIN
+	IF (new.street NOT IN (SELECT street FROM street))
+    THEN SET new.street = NULL;
+	END IF;
+END //
+
+DELIMITER ;
+
+DELIMITER //
+CREATE TRIGGER before_update_pharmacy_medicine
+BEFORE UPDATE
+ON pharmacy_medicine FOR EACH ROW
+BEGIN
+	IF (new.pharmacy_id NOT IN (SELECT id FROM pharmacy)) 
+    THEN SIGNAL SQLSTATE '45000'
+		SET MESSAGE_TEXT = "There is no such pharmacy!";
+	END IF;
+    
+    IF (new.medicine_id NOT IN (SELECT id FROM medicine)) 
+    THEN SIGNAL SQLSTATE '45000'
+		SET MESSAGE_TEXT = "There is no such medicine!";
+	END IF;
 END //
 
 DELIMITER ;
@@ -128,10 +194,68 @@ BEGIN
     DELETE FROM medicine_zone WHERE old.id = medicine_id;
 END //
 
+DELIMITER;
+
 DELIMITER //
 CREATE TRIGGER after_delete_zone
 AFTER DELETE
 ON zone FOR EACH ROW
 BEGIN
     DELETE FROM medicine_zone WHERE old.id = zone_id;
+END  //
+
+DELIMITER;
+
+-- validation
+
+DELIMITER //
+CREATE TRIGGER employee_validation_insert
+BEFORE INSERT
+ON employee FOR EACH ROW
+BEGIN
+	IF (new.identity_number RLIKE '00$' )
+    THEN SIGNAL SQLSTATE '45000'
+		SET MESSAGE_TEXT = "Choose another identity number";
+	END IF;
 END //
+
+DELIMITER ;
+
+DELIMITER //
+CREATE TRIGGER employee_validation_update
+BEFORE UPDATE
+ON employee FOR EACH ROW
+BEGIN
+	IF (new.identity_number RLIKE '00$' )
+    THEN SIGNAL SQLSTATE '45000'
+		SET MESSAGE_TEXT = "Choose another identity number";
+	END IF;
+END //
+
+DELIMITER ;
+
+DELIMITER //
+CREATE TRIGGER medicine_validation_insert
+BEFORE INSERT
+ON medicine FOR EACH ROW
+BEGIN
+	IF NOT(new.ministry_code RLIKE '^[^МП][^МП]-[0-9][0-9][0-9]-[0-9][0-9]$' )
+    THEN SIGNAL SQLSTATE '45000'
+		SET MESSAGE_TEXT = "Ministry code format: 2 довільні букви, окрім М та П - 3 цифри - 2 цифри";
+	END IF;
+END //
+
+DELIMITER ;
+
+DELIMITER //
+CREATE TRIGGER medicine_validation_update
+BEFORE UPDATE
+ON medicine FOR EACH ROW
+BEGIN
+	IF NOT(new.ministry_code RLIKE '^[^МП][^МП]-[0-9][0-9][0-9]-[0-9][0-9]$' )
+    THEN SIGNAL SQLSTATE '45000'
+		SET MESSAGE_TEXT = "Ministry code format: 2 довільні букви, окрім М та П - 3 цифри - 2 цифри";
+	END IF;
+END //
+
+DELIMITER ;
